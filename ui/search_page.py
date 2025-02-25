a5a0d25bd660a9a6e5a778a2cc7137b929900df0
sarah-innis <sei3hk@virginia.edu>
2025-02-24 17:15:15 -0800

    Added images to search results

--- ui/search_page.py
+++ ui/search_page.py
@@ -17,10 +17,30 @@ if response.status_code == 200:
 else:
     print(f"Failed to fetch data: {response.status_code}")
 
+response = requests.get('https://raw.githubusercontent.com/Ivaldivi/DATA515-Image-Classification/refs/heads/main/data/landmarks_washington_clean_images.csv')
+if response.status_code == 200:
+    pics_df = pd.read_csv(StringIO(response.text), sep=',')
+    print("Data loaded successfully!")
+else:
+    print(f"Failed to fetch data: {response.status_code}")
+
+# left join landmarks_df and pics_df on 'landmark_id' column
+landmarks_df_join = landmarks_df.merge(pics_df, how='left', on='landmark_id')
+
 text_search = st.text_input("Search for a landmark by name", "")
-search_results = landmarks_df[landmarks_df['name'].str.contains(text_search, case=False, na=False)]
+
+# this gives me multiple rows per landmarks as there are multiple images. Think about whether you want this or not
+search_results = landmarks_df_join[landmarks_df_join['name'].str.contains(text_search, case=False, na=False)]
+
 
 if text_search:
     st.write(search_results)
 else:
     st.write("Enter a landmark name to search for it.")
+
+# Display images
+n_pics_per_row = 3
+if text_search:
+    for i in range(0, len(search_results), n_pics_per_row):
+        row = search_results.iloc[i:i + n_pics_per_row]
+        st.image(row['url'].tolist(), width=200, caption=row['name'].tolist())
