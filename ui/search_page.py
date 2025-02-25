d70c069925972b1bd0c5129a5de3939315e0dd37
sarah-innis <sei3hk@virginia.edu>
2025-02-24 16:55:50 -0800

    Added function to filter data frame by search term

--- ui/search_page.py
+++ ui/search_page.py
@@ -9,10 +9,18 @@ st.set_page_config(page_title = "Washington Landmarks Search", page_icon = ":mag
 st.title("Washington Landmarks Search")
 
 # Connect to data for search
+# should I do a try catch here? do we want the exception to go to streamlit or terminal
 response = requests.get('https://raw.githubusercontent.com/Ivaldivi/DATA515-Image-Classification/refs/heads/main/data/landmarks_washington_full.csv')
 if response.status_code == 200:
-    landmarks_df = pd.read_csv(StringIO(response.text), sep='\t')
-    st.write("Data loaded successfully!")
+    landmarks_df = pd.read_csv(StringIO(response.text), sep=',')
+    print("Data loaded successfully!")
 else:
-    st.write(f"Failed to fetch data: {response.status_code}")
+    print(f"Failed to fetch data: {response.status_code}")
 
+text_search = st.text_input("Search for a landmark by name", "")
+search_results = landmarks_df[landmarks_df['name'].str.contains(text_search, case=False, na=False)]
+
+if text_search:
+    st.write(search_results)
+else:
+    st.write("Enter a landmark name to search for it.")
