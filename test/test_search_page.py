4eadb3c64b41045e4289d4d083cd46f52448fd3c
sarah-innis <sei3hk@virginia.edu>
2025-02-27 20:50:05 -0800

    enabled build_test pylint and modify get_data_from_csv

--- test/test_search_page.py
+++ test/test_search_page.py
@@ -1,15 +1,31 @@
+"""
+This file contains the unit tests for the getDataFromCSV function 
+"""
+
 import unittest
-from ui.helpers.getDataFromCSV import getDataFromCSV
+
 import pandas as pd
 
-class TestSearchPage(unittest.TestCase):
+from ui.helpers.get_data_from_csv import get_data_from_csv
 
-    def test_getDataFromCSV_goodURL(self):
-        actual = getDataFromCSV('https://raw.githubusercontent.com/Ivaldivi/DATA515-Image-Classification/refs/heads/main/data/landmarks_washington_full.csv')
+class TestSearchPage(unittest.TestCase):
+    """
+    This class contains the unit tests for the getDataFromCSV function
+    """
+
+    def test_get_data_from_csv_good_file_path(self):
+        """function to test the getDataFromCSV function
+        with valid file path
+        """
+
+        actual = get_data_from_csv('data/landmarks_washington_full.csv')
         self.assertIsInstance(actual, pd.DataFrame, "Output is not a pandas DataFrame")
 
-    def test_getDataFromCSV_badURL(self):
-        actual = getDataFromCSV('https://raw.githubusercontent.com/Ivaldivi/DATA515-Image-Classification/refs/heads/main/data/landmarks_washington.csv')
+    def test_get_data_from_csv_bad_file_path(self):
+        """function to test the getDataFromCSV function
+        with invalid file path
+        """
+        actual = get_data_from_csv('../data/landmarks_washington.csv')
         self.assertIsNone(actual, "Output is not None")
 
 if __name__ == '__main__':
---
+++ ui/helpers/get_data_from_csv.py
@@ -0,0 +1,20 @@
+"""
+This module is used to fetch data from a CSV file.
+"""
+
+import pandas as pd
+
+def get_data_from_csv(file_path):
+    """
+    Fetches data from a CSV file and returns it as a pandas DataFrame.
+
+    input: file_path (str) - the path to the CSV file
+    output: df (pd.DataFrame) - the data in the CSV file as a pandas DataFrame
+    edge case: if the file is not found, return None
+    """
+    try:
+        df = pd.read_csv(file_path, sep=',')
+        return df  
+    except FileNotFoundError as e:
+        print(f"Failed to fetch data: {file_path}")
+        return None  
