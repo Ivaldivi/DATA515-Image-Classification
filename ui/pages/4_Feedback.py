da502c0ade6462dc8885ef25556c3097392569e4
Ivaldivi <izzy@thevaldivias.com>
2025-03-04 16:21:52 -0800

    Add bones of EmailJS functionality

--- ui/pages/4_Feedback.py
+++ ui/pages/4_Feedback.py
@@ -4,7 +4,10 @@ This file contains the code for the Feedback page of the Streamlit app.
 # pylint: disable=invalid-name
 # Pylint attribute disabled due to Streamlit multi-page naming conventions
 
+import requests
+
 import streamlit as st
+
 
 st.set_page_config(
     page_title="Feedback - WA Landmark Classifier",
@@ -20,16 +23,46 @@ st.markdown(
     "landmark, please include it in the form.",
     unsafe_allow_html = True
 )
+EMAIL_API_URL = "https://api.emailjs.com/api/v1.0/email/send"
 
-with st.form(key='feedback_form', clear_on_submit=True):
+def send_email(name, email, user_feedback):
+    """
+    Send an email to the joint inbox with the user's feedback.
+    """
+    payload = {
+        "service_id": "walandmark_feedback",
+        "template_id": "template_3f5hfpd",
+        "user_id": "5caFDMvUBAm_o4TIH",
+        "template_params": {
+            "name": name,
+            "email": email,
+            "message": user_feedback
+        }
+    }
+
+    response = requests.post(EMAIL_API_URL, json=payload)
+    print("Testing the print statment...")
+    print(response.status_code)
+    if response.status_code == 200:
+        st.success("Feedback sent successfully!")
+    else:
+        st.error("Failed to send feedback. Please try again later.")
+
+
+with st.form(key='general_feedback_form', clear_on_submit=True):
+    name = st.text_input('Name (optional):')
+    email = st.text_input('Email (optional):')
     user_feedback = st.text_area('Please share your feedback:')
+
     image = st.file_uploader(label='Upload relevant files (optional):',
                          accept_multiple_files=True,
                          help='''If there is an image that is relevant to
                          your feedback, please provide it here. You may also
-                         upload other types of files.''')
+                         upload a .docx or pdf file with your feedback.''')
     submitted = st.form_submit_button('Submit')
 
 if submitted:
     st.success('Success. Thank you for your feedback!')
-    # Send an email with user feedback
+    # send email to joint inbox: 
+    send_email(name, email, user_feedback)
+
