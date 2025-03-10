bc03afa5bfc78d8b1a50ff23e281cda2813c82bf
sarah-innis <sei3hk@virginia.edu>
2025-03-09 19:40:19 -0700

    altered file paths to allow streamlit walandmarks/ui/Home.py to run in root

--- walandmarks/test/test_get_data_from_csv.py
+++ walandmarks/test/test_get_data_from_csv.py
@@ -6,7 +6,7 @@ import unittest
 
 import pandas as pd
 
-from ui.helpers.get_data_from_csv import get_data_from_csv
+from walandmarks.ui.helpers.get_data_from_csv import get_data_from_csv
 
 class TestGetDataFromCsv(unittest.TestCase):
     """
@@ -18,14 +18,14 @@ class TestGetDataFromCsv(unittest.TestCase):
         function to test get_data_from_csv function with valid file path
         """
 
-        actual = get_data_from_csv('data/landmarks_washington_full.csv')
+        actual = get_data_from_csv('walandmarks/data/landmarks_washington_full.csv')
         self.assertIsInstance(actual, pd.DataFrame, "Output is not a pandas DataFrame")
 
     def test_get_data_from_csv_bad_file_path(self):
         """
         function to test the get_data_from_csv function with invalid file path
         """
-        actual = get_data_from_csv('../data/landmarks_washington.csv')
+        actual = get_data_from_csv('data/landmarks_washington.csv')
         self.assertIsNone(actual, "Output is not None")
 
 if __name__ == '__main__':
