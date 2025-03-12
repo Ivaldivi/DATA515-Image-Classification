5b13612b6ed5751cd054256a59e01a331ae5f026
anniestaker <astaker@uw.edu>
2025-03-12 13:29:10 -0700

    add error testing for load_landmarks

--- walandmarks/test/test_load_landmarks.py
+++ walandmarks/test/test_load_landmarks.py
@@ -38,3 +38,13 @@ class TestLoadLandmarks(unittest.TestCase):
         landmark_classes_path = "walandmarks/data/landmark_classes.csv"
         landmarks = load_landmarks(landmark_classes_path)
         self.assertEqual(296, len(landmarks))
+
+    def test_load_landmarks_bad_path(self):
+        landmark_classes_path = "walandmarks/bad-folder/landmark_classes.csv"
+        with self.assertRaises(TypeError):
+            load_landmarks(landmark_classes_path)
+
+    def test_load_landmarks_wrong_filetype(self):
+        landmark_classes_path = "walandmarks/images/seattle-skyline.jpg"
+        with self.assertRaises(UnicodeDecodeError):
+            load_landmarks(landmark_classes_path)
