455367f8bb6768b1a3bb3513659323a97376ccfa
sarah-innis <sei3hk@virginia.edu>
2025-03-11 20:51:14 -0700

    updated file paths and corrected snack case in function name

--- walandmarks/test/test_get_data_from_csv.py
+++ walandmarks/test/test_get_data_from_csv.py
@@ -1,5 +1,5 @@
 """
-This file contains the unit tests for the getDataFromCSV function 
+This file contains the unit tests for the helpers/get_data_from_csv function 
 """
 
 import unittest
@@ -10,22 +10,22 @@ from walandmarks.ui.helpers.get_data_from_csv import get_data_from_csv
 
 class TestGetDataFromCsv(unittest.TestCase):
     """
-    This class contains the unit tests for the getDataFromCSV function
+    This class contains the unit tests for the helpers/get_data_from_csv function
     """
 
     def test_get_data_from_csv_good_file_path(self):
-        """function to test the getDataFromCSV function
+        """function to test the get_data_from_csv
         with valid file path
         """
 
-        actual = get_data_from_csv('data/landmarks_washington_full.csv')
+        actual = get_data_from_csv('walandmarks/data/landmarks_washington_full.csv')
         self.assertIsInstance(actual, pd.DataFrame, "Output is not a pandas DataFrame")
 
     def test_get_data_from_csv_bad_file_path(self):
-        """function to test the getDataFromCSV function
+        """function to test the get_data_from_csv
         with invalid file path
         """
-        actual = get_data_from_csv('../data/landmarks_washington.csv')
+        actual = get_data_from_csv('data/landmarks_washington.csv')
         self.assertIsNone(actual, "Output is not None")
 
 if __name__ == '__main__':
