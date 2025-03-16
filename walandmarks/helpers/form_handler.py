46b8d9fe9c5193bf72ad2782307866489262dc71
Ivaldivi <izzy@thevaldivias.com>
2025-03-15 21:14:42 -0700

    Document reason for disabling pylint reason

--- walandmarks/helpers/form_handler.py
+++ walandmarks/helpers/form_handler.py
@@ -91,6 +91,9 @@ def convert_urls_to_html(urls):
 
     images_in_html_tags = ""
     for url in urls:
+        # We had to disable the pylint consider-using-join warning here
+        # because the <img> tags produced using .join() do not properly
+        # render in the email inbox. 
         images_in_html_tags += f'<img src="{url}" width="300"><br>'  # pylint: disable=consider-using-join
     return images_in_html_tags
 
