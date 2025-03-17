2ee2d631a5934eee8f7e716094c3280fbaf4d719
Ivaldivi <izzy@thevaldivias.com>
2025-03-16 22:28:14 -0700

    Add initial tests for landmark_classification_model_training

--- walandmarks/test/test_model_training.py
+++ walandmarks/test/test_model_training.py
@@ -6,6 +6,7 @@ landmark_classification_model_training.py file.
 import unittest
 
 import keras
+import pandas as pd
 
 import walandmarks.helpers.landmark_classification_model_training as cm
 
@@ -15,7 +16,7 @@ class TestLandmarkClassificationModelTraining(unittest.TestCase):
     model training functions in the 
     landmarks_classification_model_training.py file.
     """
-    # Tests for create_model_architecture: 
+    # Tests for create_model_architecture:
     def test_model_architecture_num_classes_not_int(self):
         """
         Function to test create_model_architecture
@@ -35,7 +36,7 @@ class TestLandmarkClassificationModelTraining(unittest.TestCase):
         Function to test create_model_architecture
         raises value error when input_shape is not a tuple
         """
-        with self.assertRaises(TypeError): 
+        with self.assertRaises(TypeError):
             cm.create_model_architecture(5, 0.5, "not a tuple")
 
     def test_model_architecture_returns_keras_model(self):
@@ -46,8 +47,22 @@ class TestLandmarkClassificationModelTraining(unittest.TestCase):
         actual = cm.create_model_architecture(300, 0.5, (224, 224, 3))
         self.assertIsInstance(actual, keras.Model)
 
-    # Tests for 
+    # Tests for load_model_data():
+    def test_load_model_returns_pd_dataframe(self):
+        """
+        Function to check that load_model_data
+        returns a pandas DataFrame
+        """
+        actual = cm.load_model_data()
+        self.assertIsInstance(actual, pd.DataFrame)
 
-        
-        
-        
+    def test_load_model_data_columns(self):
+        """
+        Function to check that the datafram that's returned
+        has the correct columns. 
+        """
+        actual = cm.load_model_data()
+        expected_columns = ['file_path', 'parent_file_path', 'name', 'image_id', 'image_data']
+        actual_columns = list(actual.columns)
+        print(f'actual_columns: {actual_columns}')
+        self.assertListEqual(expected_columns, actual_columns)
