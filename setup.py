50cdcb583359384bccd0991fae6832306aae5dca
sarah-innis <sei3hk@virginia.edu>
2025-03-20 11:41:26 -0700

    removing old model files in all three setup files

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.1",
     packages=['walandmarks'],
     include_package_data = True,
-    package_data = {"walandmarks": ["data/*.csv", "images/*", "model/*.png", "model/*.keras", "model/*.md"],},
+    package_data = {"walandmarks": ["data/*.csv", "images/*", "model/*.png", "walandmarks\model\final_EfficientNetb0_WA_landmarks_model.keras", "model/*.md"],},
     install_requires=[]
 )
