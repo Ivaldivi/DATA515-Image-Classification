34b4bd22f0ebc954e70ce1495e5cfab59c592c03
sarah-innis <sei3hk@virginia.edu>
2025-03-20 11:27:32 -0700

    modify package set up files to remove old model from package

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.1",
     packages=['walandmarks'],
     include_package_data = True,
-    package_data = {"walandmarks": ["data/*.csv", "images/*", "model/*"],},
+    package_data = {"walandmarks": ["data/*.csv", "images/*", "model/*.png", "model/*.keras", "model/*.md"],},
     install_requires=[]
 )
